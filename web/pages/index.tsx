c73ad6e2b56481a2c8e26b1aad0096abfef80fcf
Quentin LAURENT <quentin.laurent@ie-concept.fr>
2020-10-28 18:25:49 +0100

    :whale: Added docker support and environment variable as configuration

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,80 +1,97 @@
-import config from "@config/config";
-import fetch from 'node-fetch'
-import { FunctionComponent } from 'react';
-import ReactMarkdown from 'react-markdown';
-import Link from 'next/link';
-import Title from '@components/Title';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
-import Container from '@components/Container';
+import Container from "@components/Container";
 import Recipes from "@components/List/Recipes";
+import Title from "@components/Title";
+import config from "@config/config";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Link from "next/link";
+import fetch from "node-fetch";
+import { FunctionComponent } from "react";
+import ReactMarkdown from "react-markdown";
 
 type HomeProps = {
   homeContent: {
-    content: string,
-},
-  recipes
-}
+    content: string;
+  };
+  recipes;
+};
 
-const Home: FunctionComponent<HomeProps> = ( { homeContent, recipes } ) => {
-
+const Home: FunctionComponent<HomeProps> = ({ homeContent, recipes }) => {
   const theme = useTheme();
-  const isSmallScreen = (!useMediaQuery(theme.breakpoints.up('sm')));
-  const isMediumScreen = (!useMediaQuery(theme.breakpoints.up('md')));
+  const isSmallScreen = !useMediaQuery(theme.breakpoints.up("sm"));
+  const isMediumScreen = !useMediaQuery(theme.breakpoints.up("md"));
 
   return (
     <>
-      <Container type={isMediumScreen === true ? 'fullAuto': 'full'} bgcolor="primary.main" >
+      <Container
+        type={isMediumScreen === true ? "fullAuto" : "full"}
+        bgcolor="primary.main"
+      >
         <Container mb={6} display="flex" justifyContent="space-between">
-            <Box display="flex" flexDirection="column" justifyContent="center" mx={1}>
-              <Title size={1}>Lorem Impsum ma antedus el yoo</Title>
-              <p style={{fontSize: "20px"}} ><ReactMarkdown source={homeContent.content} /></p>
-            </Box>
-            <Box display={isSmallScreen === true ? 'none': ''} mx={3} m={-2}>
-              <img width="350px" src="/image/food11.png" alt=""/>
-            </Box>
-            
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            mx={1}
+          >
+            <Title size={1}>Lorem Impsum ma antedus el yoo</Title>
+            <p style={{ fontSize: "20px" }}>
+              <ReactMarkdown source={homeContent.content} />
+            </p>
+          </Box>
+          <Box display={isSmallScreen === true ? "none" : ""} mx={3} m={-2}>
+            <img width="350px" src="/image/food11.png" alt="" />
+          </Box>
         </Container>
       </Container>
 
       <Container>
-      
-        <Box display="flex" alignItems="center" flexDirection={isSmallScreen === true ? 'column': 'row'} mb={2} mt={4} mx={1}>
+        <Box
+          display="flex"
+          alignItems="center"
+          flexDirection={isSmallScreen === true ? "column" : "row"}
+          mb={2}
+          mt={4}
+          mx={1}
+        >
           <Box mr={1}>
-            <Title size={2} >Dernières recettes</Title>
+            <Title size={2}>Dernières recettes</Title>
           </Box>
           <Link href="/recettes">
-            <Button variant="outlined" size="small">Voir toutes les recettes</Button>
+            <Button variant="outlined" size="small">
+              Voir toutes les recettes
+            </Button>
           </Link>
         </Box>
 
         <Recipes recipes={recipes} />
-
       </Container>
     </>
   );
-}
+};
 
 export default Home;
 
 export async function getStaticProps() {
+  console.log(config);
   // home content request
-  const reponseHome = await fetch(config.strapiUrl + '/home');
-  const jsonResponseHome = (await reponseHome.json() || [] );
+  const reponseHome = await fetch(config.configuration.strapiUrl + "/home");
+  const jsonResponseHome = (await reponseHome.json()) || [];
 
   // last receipts request
-  const reponseRecipes = await fetch(config.strapiUrl + '/recipes?_limit=4&_sort=id:DESC');
-  const jsonResponseRecipes = (await reponseRecipes.json() || [] )
-  
+  const reponseRecipes = await fetch(
+    config.configuration.strapiUrl + "/recipes?_limit=4&_sort=id:DESC"
+  );
+  const jsonResponseRecipes = (await reponseRecipes.json()) || [];
+
   return {
     props: {
       homeContent: {
-          content: jsonResponseHome.content,
+        content: jsonResponseHome.content,
       },
-      recipes: jsonResponseRecipes
-    }
-
-  }
+      recipes: jsonResponseRecipes,
+    },
+  };
 }
